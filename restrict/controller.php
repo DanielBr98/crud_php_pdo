948ea7ee388c46fde2ca4102bd5d76a6d6af7b0a
Daniel Lima <43521892+DanielBr98@users.noreply.github.com>
2020-09-24 13:45:23 -0300

    Add files via upload

--- restrict/controller.php
+++ restrict/controller.php
@@ -8,43 +8,56 @@ if (isset($form)) {
     $dataBase = new DataBase($conn);
     session_start();
 
-    if ($target == 'register') {
+    switch ($target) {
 
-        $insert = $dataBase->register($name, $email, $phoneNumber);
-        if ($insert == true) {
-            $_SESSION['msg'] = "<p id='p1'>Data sent successfully!</p>";
-            header("Location: ../index.php?page=register");
-        } else {
-            $_SESSION['msg'] = "<p id='p2'>Try again!</p>";
-            header("Location: ../index.php?page=register");
-        }
-    } elseif ($target == 'edit') {
+        case "register":
 
-        $update = $dataBase->edit($name, $email, $phoneNumber, $id);
-        if ($update == true) {
-            $_SESSION['msg'] = "<p id='p1'>Data sent successfully!</p>";
-            header("Location: ../index.php?page=edit&id=$id");
-        } else {
-            $_SESSION['msg'] = "<p id='p2'>Try again!</p>";
-            header("Location: ../index.php?page=edit&id=$id");
-        }
-    } elseif ($target == 'deleteUser') {
+            $insert = $dataBase->register($name, $email, $phoneNumber);
+            if ($insert == true) {
+                $_SESSION['msg'] = "<p id='p1'>Data sent successfully!</p>";
+                header("Location: ../index.php?page=register");
+            } else {
+                $_SESSION['msg'] = "<p id='p2'>Try again!</p>";
+                header("Location: ../index.php?page=register");
+            }
 
-        $delete = $dataBase->delete($id);
-        if ($delete == true) {
-            $_SESSION['msg'] = "<p id='p1'>Data deleted successfully!</p>";
-            header("Location: ../index.php?page=list");
-        } else {
-            $_SESSION['msg'] = "<p id='p2'>Try again!</p>";
-            header("Location: ../index.php?page=list");
-        }
+            break;
+
+        case "edit":
+
+            $update = $dataBase->edit($name, $email, $phoneNumber, $id);
+            if ($update == true) {
+                $_SESSION['msg'] = "<p id='p1'>Data sent successfully!</p>";
+                header("Location: ../index.php?page=edit&id=$id");
+            } else {
+                $_SESSION['msg'] = "<p id='p2'>Try again!</p>";
+                header("Location: ../index.php?page=edit&id=$id");
+            }
+
+            break;
+
+        case "deleteUser":
+
+            $delete = $dataBase->delete($id);
+            if ($delete == true) {
+                $_SESSION['msg'] = "<p id='p1'>Data deleted successfully!</p>";
+                header("Location: ../index.php?page=list");
+            } else {
+                $_SESSION['msg'] = "<p id='p2'>Try again!</p>";
+                header("Location: ../index.php?page=list");
+            }
+
+            break;
+
+        default:
+            header("Location: ../index.php");
     }
 } elseif (isset($page) || @$page == '') {
 
     @$pagina = $page;
-    if ($pagina == '' || $pagina == 'index.php')
+    if ($pagina == '' || $pagina == 'index.php') {
         include('list.php');
-    elseif (file_exists($pagina . '.php')) {
+    } elseif (file_exists($pagina . '.php')) {
         include($pagina . '.php');
     }
 } else {
